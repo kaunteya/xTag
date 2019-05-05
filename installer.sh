a9377e0c8edf4b737720e3c52035844e46a7ddc0
Kaunteya <>
2019-05-05 17:41:04 +0530

    Updated installer

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 ## Clone the repo
 git clone https://github.com/kaunteya/xTag.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
-cd git-checkout-interactive
+cd xTag
 make install || { echo >&2 "Clone failed with $?"; exit 1; }
 cd ..
 rm -rf xTag
